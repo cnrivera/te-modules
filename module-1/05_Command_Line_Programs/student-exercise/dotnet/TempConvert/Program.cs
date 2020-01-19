3619001b2671cfcde5cfc5a7bfe717116c9337a0
Christina Rivera <cnrivera@gmail.com>
2020-01-19 01:21:48 -0500

    Partial update M1 05 Command Line Exercises, Temp Convert program

--- module-1/05_Command_Line_Programs/student-exercise/dotnet/TempConvert/Program.cs
+++ module-1/05_Command_Line_Programs/student-exercise/dotnet/TempConvert/Program.cs
@@ -6,7 +6,27 @@ namespace TempConvert
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter the temperature:");
+            string strGetTemp = Console.ReadLine();
+            int intTemp = int.Parse(strGetTemp);
+
+            Console.WriteLine("Is the temperature in (C)elsius or (F)ahrenheit?");
+            string strTempScale = Console.ReadLine();
+
+            double tempCelsiusConvert = (intTemp - 32) / 1.8;
+            double tempFahrenheitConvert = intTemp * 1.8 + 32;
+
+            if (strTempScale == "F")
+            {
+                Console.WriteLine(intTemp + "F is " + Math.Floor(tempCelsiusConvert) + "C.");
+            }
+            else if (strTempScale == "C")
+            {
+                Console.WriteLine(intTemp + "C is " + Math.Floor(tempFahrenheitConvert) + "F.");
+            }
+           
         }
+
+        
     }
 }
