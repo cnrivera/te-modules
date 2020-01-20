317a7b19d75e57ae34db4c62cfd131ab82eb824e
Christina Rivera <cnrivera@gmail.com>
2020-01-20 08:33:04 -0500

    Completed M1 05 Command Line Exercises

--- module-1/05_Command_Line_Programs/student-exercise/dotnet/DecimalToBinary/Program.cs
+++ module-1/05_Command_Line_Programs/student-exercise/dotnet/DecimalToBinary/Program.cs
@@ -8,25 +8,30 @@ namespace DecimalToBinary
         {
             //PROGRAM NOT COMPLETE
 
-            Console.Write("Please enter a series of decimal values (separated by spaces): ");
-            string strDecValues = Console.ReadLine();
-            string[] decValuesArr = strDecValues.Split(" ");
+            Console.Write("Please enter a series of decimal values (separated by spaces): "); //request information from user
+            string strDecValues = Console.ReadLine(); //store input int a string
+            string[] decValuesArr = strDecValues.Split(" "); //split string into an array using space char
+            int[] decToBinary = Array.ConvertAll(decValuesArr,int.Parse);
 
-            //for (int i = 0; i < decValuesArr.Length; i++)
-            //{
-                string ConvertNumberToBinary(int base10)
-                {
-                    string binary = "";
-                    while (base10 > 0)
-                    {
-                        binary = (base10 % 2) + binary; base10 = base10 / 2;
-                    }
-                    return binary;
+            for (int i = 0; i < decValuesArr.Length; i++)
+            {
+                Console.WriteLine(decToBinary[i] + "in binary is " + ConvertNumberToBinary(decToBinary[i]));
 
-                    //int decToBinary = int.Parse(decValuesArr[i]);
-                    //Console.WriteLine( + "in binary is " + );
-                //}
             }
+
+            //int decToBinary = int.Parse(decValuesArr[i]);
+            //Console.WriteLine( + "in binary is " + );
+            //}
+        }
+        private static string ConvertNumberToBinary(int base10) // program says local function declared but never used?
+        {
+            string binary = "";
+            while (base10 > 0)
+            {
+                binary = (base10 % 2) + binary;
+                base10 = base10 / 2;
+            }
+            return binary;
         }
     }
 }
