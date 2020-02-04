96ec39ff2941a49b56423b48045b1475386f83a8
Christina Rivera <cnrivera@gmail.com>
2020-02-04 12:46:41 -0500

    Update IO Reading exercises

--- module-1/17_FileIO_Reading_in/student-exercise/dotnet/WordSearch/Program.cs
+++ module-1/17_FileIO_Reading_in/student-exercise/dotnet/WordSearch/Program.cs
@@ -8,33 +8,54 @@ namespace WordSearch
         public static void Main(string[] args)
         {
 
-            Console.WriteLine("Enter the word to search for.");
-            string wordToSearch = Console.ReadLine();
-
-            Console.WriteLine("Enter the full file path of the file to search.");
+            Console.WriteLine("Enter the full file path of the file to search: ");
             string filePath = Console.ReadLine();
             // TK Add instructions to catch problems in command path
 
-                try
-                {
+            while (!File.Exists(filePath))
+            {
+                Console.WriteLine("Invalid file name. Please re-enter: ");
+                filePath = Console.ReadLine();
+            }
+            
+            Console.Write("Enter the word to search for: ");
+            string wordToSearch = Console.ReadLine();
+
+            
+            Console.Write("Should the search be case sensitive? Y/N? ");
+            string caseSensitive = Console.ReadLine().ToUpper();
+
+            
+            try
+            {
                     using (StreamReader sr = new StreamReader(filePath))
                     {
                         int lineNumber = 0;
+                        bool found = false;
                         while (!sr.EndOfStream)
                         {
                             string line = sr.ReadLine();
                             lineNumber++;
 
                             int position = line.IndexOf(wordToSearch);
+                            if (caseSensitive.Equals("N"))
+                        {
+                            position = line.ToLower().IndexOf(wordToSearch.ToLower());
+                        }
+
+
                             if (position != -1)
                             {
-
+                            found = true;
                                 Console.WriteLine("{0}: {1}", lineNumber, line);
 
                             }
 
                         }
-
+                        if (!found)
+                    {
+                        Console.WriteLine("Word not found.");
+                    }
                     }
                 }
                 catch (IOException e)
