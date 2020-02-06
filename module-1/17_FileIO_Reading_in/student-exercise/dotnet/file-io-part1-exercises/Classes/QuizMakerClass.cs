ba4180b5cec43df8e2b2482aaf57b5ff2495030f
Christina Rivera <cnrivera@gmail.com>
2020-02-06 08:52:31 -0500

    Update to Quiz Maker homework

--- module-1/17_FileIO_Reading_in/student-exercise/dotnet/file-io-part1-exercises/Classes/QuizMakerClass.cs
+++ module-1/17_FileIO_Reading_in/student-exercise/dotnet/file-io-part1-exercises/Classes/QuizMakerClass.cs
@@ -4,9 +4,34 @@ using System.Text;
 
 namespace file_io_part1_exercises.Classes
 {
-    class QuizMakerClass
+    public class QuizMakerClass
     {
+        public string Question { get; }
+        public List<string> Answer { get;}
+        private string correctAnswer;
+        
+        public QuizMakerClass(string question, string correctAnswer, List<string> answer)
+        {
+            Question = question;
+            this.correctAnswer = correctAnswer;
+            Answer = answer;
 
+        }
 
+       public bool isAnswerCorrect (string choice)
+        {
+            if (choice == correctAnswer)
+            {
+                return true;
+            }
+            return false;
+            
+        }
+
+        public override string ToString()
+        {
+            string result = $"{Question} \n 1. {Answer[0]} \n 2. {Answer[1]} \n 3. {Answer[2]} \n 4. {Answer[3]} \n";
+            return result;
+        }
     }
 }
--- module-1/17_FileIO_Reading_in/student-exercise/dotnet/file-io-part1-exercises/Program.cs
+++ module-1/17_FileIO_Reading_in/student-exercise/dotnet/file-io-part1-exercises/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using file_io_part1_exercises.Classes;
 
 namespace file_io_part1_exercises
 {
@@ -6,7 +10,64 @@ namespace file_io_part1_exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = Environment.CurrentDirectory;
+            string file = "sample-quiz-file.txt";
+            string filepath = Path.Combine(path, file);
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(filepath))
+                {
+                   while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        string[] originalList = line.Split("|");
+                        string question = originalList[0];
+                        string[] answerArray = { originalList[1], originalList[2], originalList[3], originalList[4] };
+                        string correctAnswer = "";
+                        
+                        Console.WriteLine(question);
+                        
+                        for (int i = 1; i < originalList.Length; i++)
+                        {
+                            bool hasStar = originalList[i].EndsWith('*');
+                            while (originalList[i].EndsWith('*'))
+                            {
+                                originalList[i] = originalList[i].TrimEnd('*');
+                                correctAnswer = originalList[i].ToLower();
+                            }
+                           
+                            Console.WriteLine(originalList[i]);
+                        }
+                       
+                        //QuizMakerClass qmc = new QuizMakerClass(question, correctAnswer, answerArray.ToList());
+                        //Console.WriteLine(qmc);
+                        string userInput = Console.ReadLine();
+
+                        if (userInput == correctAnswer)
+                        {
+                            Console.WriteLine("Correct!");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry, incorrect.");
+                        }
+
+                        
+
+
+                    }
+
+
+                };
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+            Console.ReadLine();
         }
     }
 }
