d132366c797a4e1d6ba97680d2aaba45919bd8be
Christina Rivera <cnrivera@gmail.com>
2020-03-06 09:13:11 -0500

    Completed M3 W8 D4 homework

--- module-3/09-Data-Validation-and-View-Models/student-exercise/dotnet/Validation.Web/Controllers/UsersController.cs
+++ module-3/09-Data-Validation-and-View-Models/student-exercise/dotnet/Validation.Web/Controllers/UsersController.cs
@@ -20,19 +20,55 @@ namespace Validation.Web.Controllers
         // GET: User/Register
         // Return the empty registration view
 
+
+
         // POST: User/Register
         // Validate the model and redirect to confirmation (if successful) or return the 
         // registration view (if validation fails)        
 
+
+
         // GET: User/Login
         // Return the empty login view
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
 
         // POST: User/Login  
         // Validate the model and redirect to login (if successful) or return the 
         // login view (if validation fails)
+        [HttpPost]
+        public IActionResult LoginConfirmation(LoginViewModel login)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Login");
+            }
+
+            return View();
+        }
+
 
         // GET: User/Confirmation
         // Return the confirmation view
 
+        [HttpGet]
+        public IActionResult NewUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewUserConfirmation(RegistrationViewModel newUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("NewUser");
+            }
+
+            return View();
+        }
     }
 }
--- module-3/09-Data-Validation-and-View-Models/student-exercise/dotnet/Validation.Web/Models/RegistrationViewModel.cs
+++ module-3/09-Data-Validation-and-View-Models/student-exercise/dotnet/Validation.Web/Models/RegistrationViewModel.cs
@@ -8,6 +8,36 @@ namespace Validation.Web.Models
 {
     public class RegistrationViewModel
     {
-        
+        [Display(Name = "First Name")]
+        [Required, StringLength(20)]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required, StringLength(20)]
+        public string LastName { get; set; }
+
+        [Display(Name = "E-mail")]
+        [Required, EmailAddress]
+        public string NewEmail { get; set; }
+
+        [Display(Name = "Confirm E-mail")]
+        [Required, EmailAddress, Compare("NewEmail", ErrorMessage = "Password does not match")]
+        public string ConfirmEmail { get; set; }
+
+        [Display(Name = "Password")]
+        [Required, StringLength(50, MinimumLength=8), DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [Required, DataType(DataType.Password), Compare("NewPassword", ErrorMessage = "Password does not match")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "Birthday")]
+        [Required]
+        public DateTime Birthday { get; set; }
+
+        [Display(Name = "# of Tickets")]
+        [Required, Range(1, 10, ErrorMessage = "Please enter amount between 1 and 10")]
+        public int NumTickets { get; set; }
     }
 }
